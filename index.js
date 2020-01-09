c8734bd56ead408bc99010bb4627224d07e90f94
Andr3wRiv3rs <andrewarivers@gmail.com>
2020-01-08 18:16:56 -0800

    Unverified system

--- index.js
+++ index.js
@@ -1,33 +1,60 @@
 require('dotenv').config()
 
-const { MESSAGE, ROLE, CHANNEL, GUILD } = process.env
-
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-client.on('ready', async ( ) => {
-    console.log(`Logged in as ${client.user.tag}!`)
-    client.user.setStatus('offline')
+client.on('ready', ( ) => console.log(`Logged in as ${client.user.tag}!`))
 
-    await client.channels.get( CHANNEL ).fetchMessage( MESSAGE )
+client.on('messageReactionAdd', async ( { message: { channel } }, user ) => {
+    if (/rules/.test(channel.name)) {
+        await channel.guild
+            .member(user)
+            .removeRole(process.env.ROLE)
+            .catch(console.error)
+    }
+})
 
-    client.on('messageReactionAdd', async ( Reaction, User ) => {
-        try {
-            const Member = await client.guilds.get( GUILD ).fetchMember(User)
-            await Member.addRole( ROLE )
-        } catch (error) {
-            console.error(error, User.id)
-        }
-    })
+client.on('messageReactionRemove', async ( { message: { channel } }, user ) => {
+    if (/rules/.test(channel.name)) {
+        await channel.guild
+            .member(user)
+            .addRole(process.env.ROLE)
+            .catch(console.error)
+    }
+})
 
-    client.on('messageReactionRemove', async ( Reaction, User ) => {
-        try {
-            const Member = await client.guilds.get( GUILD ).fetchMember(User)
-            await Member.removeRole( ROLE )
-        } catch (error) {
-            console.error(error, User.id)
-        }
-    })
+client.on('guildMemberAdd', async member => {
+    await member
+        .setRoles([ process.env.ROLE ])
+        .catch(console.error)
+});
+
+client.on('raw', ({ d: data, t: event }) => {
+    if (['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(event)) {
+        const { channel_id, user_id, message_id, emoji } = data
+
+        const channel = client.channels.get(channel_id)
+
+        if (!channel.messages.has(message_id)) channel.fetchMessage(
+            message_id
+        ).then( message => {
+            const reaction = message.reactions.get(
+                emoji.id ? `${emoji.name}:${emoji.id}` : emoji.name 
+            )
+
+            const user = client.users.get(user_id)
+
+            if (reaction) reaction.users.set(user_id, user)
+            
+            client.emit( 
+                event === 'MESSAGE_REACTION_ADD' 
+                    ? 'messageReactionAdd' 
+                    : 'messageReactionRemove', 
+                reaction, 
+                user
+            )
+        })
+    }
 })
 
 client.login(process.env.TOKEN)
